15a5d68a65dbad6e5865e10dc1f8831a00c59130
hsnshehata <hsn.shehata@gmail.com>
2025-09-05 18:18:18 +0300

    Update ReceiptPrint.js

--- client/src/pages/ReceiptPrint.js
+++ client/src/pages/ReceiptPrint.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import QRCode from 'qrcode.react';
 import { Table } from 'react-bootstrap';
-import '../src/App.css';
+import 'client/src/App.css';
 
 const ReceiptPrint = ({ data, type }) => {
   if (!data || !type) {
