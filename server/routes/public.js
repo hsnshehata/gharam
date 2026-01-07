59b83f2bd013388812b3ee557bb486b88dffb044
hsnshehata <hsn.shehata@gmail.com>
2026-01-07 07:28:31 +0200

    Update public.js

--- server/routes/public.js
+++ server/routes/public.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const publicController = require('../controllers/publicController');
 
 router.get('/availability', publicController.checkAvailability);
+router.get('/google-reviews', publicController.getGoogleReviews);
 
 module.exports = router;
