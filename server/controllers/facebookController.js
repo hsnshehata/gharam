ba61328b5622af94367acee03fed646c7da7f5be
hsnshehata <hsn,shehata@gmail.com>
2026-02-12 13:50:22 +0200

    Fix Facebook API - remove comments.data access

--- server/controllers/facebookController.js
+++ server/controllers/facebookController.js
@@ -43,13 +43,7 @@ const syncFacebookPosts = async (req, res) => {
 		// تحديد نوع البوست (صورة أو فيديو)
 		const isVideo = post.type === 'video';
 
-		const comments = post.comments?.data?.map((c) => ({
-			id: c.id,
-			name: c.from?.name || 'Guest',
-			message: c.message,
-			createdTime: c.created_time,
-			picture: c.from?.picture?.data?.url
-		})) || [];
+		const comments = [];
 
 		const postData = {
 			facebookId: post.id,
