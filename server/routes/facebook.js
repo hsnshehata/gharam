7f5cc550aa14a0ae864c037206528f4dace81859
hsnshehata <hsn.shehata@gmail.com>
2026-02-12 12:19:37 +0200

    Update facebook.js

--- server/routes/facebook.js
+++ server/routes/facebook.js
@@ -12,5 +12,7 @@ router.get('/feed', getFacebookFeed);
 
 // Admin endpoint - تحديث يدوي
 router.post('/sync', manualSyncPosts);
+// دعم GET عشان يتجرب من المتصفح بسهولة
+router.get('/sync', manualSyncPosts);
 
 module.exports = router;
