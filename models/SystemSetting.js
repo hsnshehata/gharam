9fc74f29878cb27d62702d2cca3b2e7b1aa06a68
hsnshehata <hsn.shehata@gmail.com>
2026-04-10 02:03:17 +0200

    Delete models directory

--- models/SystemSetting.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const systemSettingSchema = new mongoose.Schema({
-  key: { type: String, required: true, unique: true },
-  value: { type: mongoose.Schema.Types.Mixed, default: null },
-  updatedAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('SystemSetting', systemSettingSchema);
