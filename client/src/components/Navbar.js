60c6dd179ec8cc93970ff587c3c73478c6decaea
hsnshehata <hsn.shehata@gmail.com>
2026-04-15 00:43:38 +0200

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -61,6 +61,7 @@ function Navbar({ user, setUser }) {
               <NavDropdown title="المحتوى" id="content-nav-dropdown">
                 <NavDropdown.Item as={Link} to="/gallery-admin" onClick={closeMenu}>المعرض</NavDropdown.Item>
                 <NavDropdown.Item as={Link} to="/ai-settings" onClick={closeMenu}>الذكاء الاصطناعي</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/model-comparison" onClick={closeMenu}>⚖️ مقارنة النماذج</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item as={Link} to="/admin/afrakoush-registry" onClick={closeMenu}>أدوات عفركوش (الجديد)</NavDropdown.Item>
               </NavDropdown>
